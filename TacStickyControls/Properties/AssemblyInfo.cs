fd00a59677cf959f3b27a8403ffdb7af5edca80c
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-04-17 07:16:14 -0400

    fixed typo in dependencies

--- TacStickyControls/Properties/AssemblyInfo.cs
+++ TacStickyControls/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("0.0.1.22")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
 
-[assembly: KSPAssembly("SlingShotter", 1, 0)]
+[assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
 [assembly: KSPAssemblyDependency("ToolbarController", 1, 0)]
